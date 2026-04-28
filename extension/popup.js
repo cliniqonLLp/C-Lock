97cd9c33d284986b56745b5700fb3669b034ba24
Sarath Vadakkedath <svadakkedath@cliniqon.com>
2026-04-28 19:59:57 +0530

    fgr

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8765";
+const API_BASE = "https://c-lock-1.onrender.com/";
 
 const loginBox = document.getElementById("loginBox");
 const sessionBox = document.getElementById("sessionBox");
